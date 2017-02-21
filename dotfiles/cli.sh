92994c1bdd7b0210ec99c3d2ede4daa5da516eba
Mac Heller-Ogden <mac@machellerogden.com>
2017-02-21 16:22:21 -0600

    more generic script for better zsh support

--- dotfiles/cli.sh
+++ dotfiles/cli.sh
@@ -2,6 +2,6 @@
 function bitcar_cli() {
     local root="${BITCAR_ROOT_DIR:-"$HOME/bitcar-repos"}"
     [ -z "$1" ] && cd "$root" && return
-    set -- bitcar "$@" && "$@" && cd "$(cat "$BITCAR_ROOT_DIR/.bitcar_target" )"
+    bitcar "$@" && cd "$(cat "$BITCAR_ROOT_DIR/.bitcar_target" )"
 }
 alias <%= alias %>=bitcar_cli
--- dotfiles/completions.sh
+++ dotfiles/completions.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-
-# Uncomment to the line below to use bash intelligent tab completion. WARNING! This will effect bash auto-complete functionality for all of our commands!
-# bind "TAB:menu-complete"
-
-__bitcar() {
-    local cur=${COMP_WORDS[COMP_CWORD]}
-    COMPREPLY=( $(bitcar --completions "$cur") )
-}
-complete -F __bitcar <%= alias %>
+if [ -z "$ZSH_VERSION" ]; then
+    __bitcar() {
+        local cur=${COMP_WORDS[COMP_CWORD]}
+        COMPREPLY=( $(bitcar --completions "$cur") )
+    }
+    complete -F __bitcar <%= alias %>
+fi
